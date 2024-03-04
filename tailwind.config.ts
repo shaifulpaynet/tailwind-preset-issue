9aae11e09853b38f03859e584c0a0c52721f03d5
Shaiful Aiman Malik <shaifulaiman@paynet.my>
2024-03-04 11:02:09 +0800

    add tailwind preset

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,9 @@
 import type { Config } from 'tailwindcss'
+import tailwindPreset from './tailwind-preset'
 
 export default {
-  content: [],
+  presets: [tailwindPreset],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
